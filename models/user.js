cf52cffe843fbc0875225258d087027cd68af873
kpetrosky <kpetrosky36@gmail.com>
2023-05-30 13:11:02 -0700

    added js files that i will need

---
+++ models/user.js
@@ -0,0 +1,12 @@
+// thoughts: [
+//     {
+//       type: Schema.Types.ObjectId,
+//       ref: 'Thought',
+//     },
+// ]
+// this will be the thoughts array
+
+
+// reactions: [reactionSchema],
+//},
+//this how to build the reaction array inside the thought model
