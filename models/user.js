ad23bcd1942469200e3db7196d123360c80bd264
kpetrosky <kpetrosky36@gmail.com>
2023-06-01 08:07:23 -0700

    added boiler temples to see what i need to code myself

--- models/user.js
+++ models/user.js
@@ -10,3 +10,45 @@
 // reactions: [reactionSchema],
 //},
 //this how to build the reaction array inside the thought model
+
+
+const { Schema, model } = require('mongoose');
+
+// Schema to create a course model
+const courseSchema = new Schema(
+  {
+    courseName: {
+      type: String,
+      required: true,
+    },
+    inPerson: {
+      type: Boolean,
+      default: true,
+    },
+    startDate: {
+      type: Date,
+      default: Date.now(),
+    },
+    endDate: {
+      type: Date,
+      // Sets a default value of 12 weeks from now
+      default: () => new Date(+new Date() + 84 * 24 * 60 * 60 * 1000),
+    },
+    students: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Student',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+const Course = model('course', courseSchema);
+
+module.exports = Course;
