0c4eec9beebe97c10bda60136444cbb591f9bd6b
kpetrosky <kpetrosky36@gmail.com>
2023-06-04 07:14:50 -0700

    added files to utils

---
+++ utils/seed.js
@@ -0,0 +1,51 @@
+const connection = require('../config/connection');
+const { reactions, thought } = require('../models');
+const { getRandomName, getRandomAssignments } = require('./data');
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+  console.log('connected');
+
+  // Drop existing reactionss
+  await reactions.deleteMany({});
+
+  // Drop existing thoughts
+  await thought.deleteMany({});
+
+  // Create empty array to hold the thoughts
+  const thoughts = [];
+
+  // Loop 20 times -- add thoughts to the thoughts array
+  for (let i = 0; i < 20; i++) {
+    // Get some random assignment objects using a helper function that we imported from ./data
+    const assignments = getRandomAssignments(20);
+
+    const fullName = getRandomName();
+    const first = fullName.split(' ')[0];
+    const last = fullName.split(' ')[1];
+    const github = `${first}${Math.floor(Math.random() * (99 - 18 + 1) + 18)}`;
+
+    thoughts.push({
+      first,
+      last,
+      github,
+      assignments,
+    });
+  }
+
+  // Add thoughts to the collection and await the results
+  await thought.collection.insertMany(thoughts);
+
+  // Add reactionss to the collection and await the results
+  await reactions.collection.insertOne({
+    reactionsName: 'UCLA',
+    inPerson: false,
+    thoughts: [...thoughts],
+  });
+
+  // Log out the seed data to indicate what should appear in the database
+  console.table(thoughts);
+  console.info('Seeding complete! 🌱');
+  process.exit(0);
+});
